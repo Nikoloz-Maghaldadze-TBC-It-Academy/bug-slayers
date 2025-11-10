4fc660fababc55756d23aa2c02d8a57870fbb7f4
gigi-giorgadze-tbc-it-academy <giorgadze.gigi2004@gmail.com>
2025-11-10 21:50:32 +0400

    feat(puzzle-2): add puzzle-2 ui

---
+++ scripts/modules/puzzle2.mjs
@@ -0,0 +1,5 @@
+const up = document.querySelector('.arrow-up');
+const down = document.querySelector('.arrow-down');
+const left = document.querySelector('.arrow-left');
+const right = document.querySelector('.arrow-right');
+
